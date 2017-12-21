65a6837f9219f78c976ba74c3339a6094d1c2747
glebwell <gleb.maltcev@yandex.ru>
2017-12-21 18:57:22 +0300

    lesson's implementation was finished

--- Concentration/Concentration/Concentration.swift
+++ Concentration/Concentration/Concentration.swift
@@ -10,6 +10,8 @@ import Foundation
 
 class Concentration {
 
+    var indexOfOneAndOnlyFaceUpCard: Int?
+
     init(numberOfPairsOfCards: Int) {
         for _ in 0..<numberOfPairsOfCards {
             let card = Card()
@@ -21,6 +23,25 @@ class Concentration {
     var cards = [Card]()
 
     func chooseCard(at index: Int) {
-        cards[index].isFaceUp = !cards[index].isFaceUp
+        if !cards[index].isMatched {
+            //cards[index].isFaceUp = !cards[index].isFaceUp
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+
     }
 }
